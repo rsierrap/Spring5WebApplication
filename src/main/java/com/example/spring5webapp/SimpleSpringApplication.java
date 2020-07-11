1b66d1c1d04894290d9d1ed1016f8b46c337bd82
Raúl Sierra Pajares <raulsierrapajares@gmail.com>
2020-07-11 11:12:11 +0200

    Initial commit

---
+++ src/main/java/com/example/spring5webapp/SimpleSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringApplication.class, args);
+	}
+
+}
